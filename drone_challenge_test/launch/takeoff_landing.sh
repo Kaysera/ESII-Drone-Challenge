ba5bd7abea1181ef5893d72cedea28b5f269804d
kaysera <folez97@gmail.com>
2016-10-16 11:18:41 +0200

    trying ubuntu again

--- drone_challenge_test/launch/takeoff_landing.sh
+++ drone_challenge_test/launch/takeoff_landing.sh
@@ -1,6 +1,6 @@
 clear
 
-#export ROS_IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+#export ROS_IP=$(ifconfig eth0 | grep "Direc. inet" | awk -F: '{print $2}' | awk '{print $1}')
 #echo '$'ROS_IP: $ROS_IP
 
 roslaunch drone_gazebo takeoff_landing.launch
